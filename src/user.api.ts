886e620bb360da485cc9eb6754958935d89e8c56
f-antonelli <antonelli.fd@gmail.com>
2024-02-06 09:24:36 -0300

    ADD: start files

---
+++ src/user.api.ts
@@ -0,0 +1,3 @@
+import { APIGatewayEvent, Context } from 'aws-lambda';
+
+export const handler = async (event: APIGatewayEvent, context: Context) => {};
---
+++ test/trocar_mvp.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TrocarMvp from '../lib/trocar_mvp-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/trocar_mvp-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TrocarMvp.TrocarMvpStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
